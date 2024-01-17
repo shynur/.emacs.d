829156d2cd225bf8521600fb2d33068480e27054
Shynur <qix@stu.ecnu.edu.cn>
2024-01-18 06:19:07 +0800

    Delete scripts directory
    Signed-off-by: Shynur <qix@stu.ecnu.edu.cn>

--- scripts/explorer.el
+++
@@ -1,9 +0,0 @@
-;;; 以正斜杠作为路径分隔符的 ‘explorer.exe’.  -*- lexical-binding: t; -*-
-;;; 调用约定: runemacs.exe -Q --load explorer.elc Z:/Path/To/Folder
-
-
-
-;; Local Variables:
-;; coding: utf-8-unix
-;; no-byte-compile: nil
-;; End:
--- scripts/make-shynur-emacs.el
+++
@@ -1,13 +0,0 @@
-#! /usr/bin/emacs -x
-;;; -*- lexical-binding: t; -*-
-
-;;; Commentary:
-;;
-;; 辅助 ‘~/.emacs.d/GNUmakefile’ 的脚本.
-
-argv
-0  ; exit code
-
-;; Local Variables:
-;; coding: utf-8-unix
-;; End:

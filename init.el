aa19a32a096864a164b57d771677cea2b91f8fcf
谢骐 <shynur@outlook.com>
2026-03-16 14:45:07 +0800

    ;

--- init.el
+++ init.el
@@ -15,6 +15,49 @@
   (add-hook 'kill-emacs-query-functions
             #'custom-prompt-customize-unsaved-options))
 
+(use-package emacs
+  :config
+  (setopt inhibit-startup-screen t)
+  (setq inhibit-startup-echo-area-message "shynur")
+  (setq inhibit-startup-echo-area-message "root")
+
+  (setopt initial-major-mode 'markdown-mode
+	  initial-scratch-message "")
+
+  (setopt default-input-method "chinese-py")
+)
+
+(use-package tool-bar
+  :if (display-graphic-p)
+  :config
+  (tool-bar-mode -1))
+
+(use-package savehist
+  :config
+  (savehist-mode))
+
+(use-package recentf
+  :config
+  (recentf-mode))
+
+(use-package saveplace
+  :config
+  (save-place-mode))
+
+(use-package frame
+  :config
+  (blink-cursor-mode -1))
+
+(use-package simple
+  :config
+  (column-number-mode)
+  (line-number-mode -1)
+  (size-indication-mode))
+
+(use-package display-line-numbers
+  :config
+  (global-display-line-numbers-mode))
+
 (use-package dockerfile-mode
   :ensure t
   :defer t)

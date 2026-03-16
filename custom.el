7a5eb4b48eb436c901931dae209426b33d670904
谢骐 <shynur@outlook.com>
2026-03-16 12:54:15 +0800

    ;

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(git-modes cmake-mode yaml-mode web-mode typescript-mode sed-mode rainbow-mode nginx-mode markdown-mode json-mode go-mode git-mode csv-mode dockerfile-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

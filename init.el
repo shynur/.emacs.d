b3d74e54d35275045a6e9e4b1b6632e2c8959c87
𝓈𝒽𝓎𝓃𝓊𝓇 <qix@stu.ecnu.edu.cn>
2026-03-18 14:38:03 +0800

    Update init.el
    Signed-off-by: 𝓈𝒽𝓎𝓃𝓊𝓇 <qix@stu.ecnu.edu.cn>

--- init.el
+++ init.el
@@ -1,59 +1,3 @@
-;;; ~shynur/.emacs.d/init.el --- A Lite Emacs Configuration  -*- coding: utf-8-unix; lexical-binding: t; -*-
-
-;; Maintained at: <https://github.com/shynur/.emacs.d/tree/lite>
-
-;; A lightweight configuration, containing only *1* file.
-;; (IOW, you can rename this file to `~/.emacs' or etc.)
-;; No packages from unknown package management sites have been added.
-;; It should be able to run
-;;   on any platform
-;;   in either TUI or GUI
-;;   as a server-clients model or a simple one.
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(custom-enabled-themes '(modus-vivendi))
- '(fringe-mode '(0) nil (fringe))
- '(global-display-line-numbers-mode t)
- '(inhibit-startup-screen t)
- '(line-number-mode nil)
- '(package-selected-packages
-   '(page-break-lines rust-mode rainbow-delimiters marginalia go-mode yasnippet company))
- '(save-place-mode t)
- '(size-indication-mode t)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Cascadia Code" :foundry "outline" :slant normal :weight regular :height 139 :width normal)))))
-
-(eval-when-compile
-  (require 'seq)
-  (require 'cl-lib))
-
-;;; Environment
-(when (eq system-type 'windows-nt)
-  (setopt exec-path (mapcar (lambda (path)
-                              (if (string-match-p "\\`%[^/%]+%" path)
-                                  (string-replace "\\" "/"
-                                                  (shell-command-to-string (format "cmd.exe /c \"(echo|set /p=%s) && exit\""
-                                                                                   path)))
-                                path))
-                            exec-path)))
-
-;;; History
-(savehist-mode)
-(recentf-mode)
-
-;;; Encoding & Decoding
-(prefer-coding-system 'utf-8-unix)
-
 ;;; UI
 (blink-cursor-mode -1)
 (push `(,(pcase system-type
@@ -81,18 +25,7 @@
                            "⏱️" (:eval (emacs-uptime "%h:%.2m")) " "
                            (pixel-scroll-precision-mode
                             nil
-                            ("🎹" (:eval (number-to-string num-input-keys)) "/" (:eval (number-to-string num-nonmacro-input-events)))))
-      icon-title-format '((:eval (prog1 #1='#:icon-title 
-                                   (set #1# (mapconcat (lambda (buffer)
-                                                         (with-current-buffer buffer
-                                                           (format "[%.4s]"
-                                                                   (buffer-name))))
-                                                       (delete-dups (mapcar (lambda (window)
-                                                                              (with-selected-window window
-                                                                                (current-buffer)))
-                                                                            (window-list)))
-                                                       "\s"))))))
-(set-face-background 'cursor "chartreuse")
+                            ("🎹" (:eval (number-to-string num-input-keys)) "/" (:eval (number-to-string num-nonmacro-input-events))))))
 
 ;;; Keyboard
 (let ((init/kbd:key-swapper (let ((init/kbd:key-swapped-terminals ()))
@@ -139,7 +72,6 @@
 
 ;;; Interavtive
 (repeat-mode)
-(setopt echo-keystrokes 0.1)
 
 ;; Local Variables:
 ;; no-byte-compile: nil
